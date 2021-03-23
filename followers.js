e9ab94f0d1980f7150e9fd69dbd118e26aef0480
Konrad Linkowski <linkowski.konrad97@gmail.com>
2021-03-23 20:22:22 +0100

    Remove constraints

--- followers.js
+++ followers.js
@@ -28,7 +28,7 @@ const request = (user, token) => new Promise((resolve, reject) => {
   }).on('error', reject)
 })
 
-const createTable = (users, numberOfRows, usersPerRow) => {
+const createTable = (users, usersPerRow) => {
   const usersHTML = users.map(user => `
   <td align="center">
     <a href="${user.html_url}">
@@ -37,21 +37,21 @@ const createTable = (users, numberOfRows, usersPerRow) => {
       ${user.name || user.login}
     </a> 
   </td>`)
-  const rows = Array(numberOfRows).fill(0).map((_, i) =>
+  const rows = Array(Math.ceil(users.length / usersPerRow)).fill(0).map((_, i) =>
     `<tr>${usersHTML.slice(i * usersPerRow, (i + 1) * usersPerRow).join('\n')}</tr>`)
   return `\n<table>${rows.join('\n')}</table>\n`
 }
 
-const getFollowers = async (user, token, numberOfRows, usersPerRow) => {
+const getFollowers = async (user, token, usersPerRow) => {
   const path = join(process.cwd(), './README.md')
   const readme = await readFile(path, 'utf-8')
-  const users = await request(user, token, numberOfRows * usersPerRow)
-  const table = createTable(users.reverse(), numberOfRows, usersPerRow)
+  const users = await request(user, token)
+  const table = createTable(users.reverse(), usersPerRow)
   const regex = /(?<=<!--START_SECTION:top-followers-->)[\s\S]*(?=<!--END_SECTION:top-followers-->)/
   const newReadme = readme.replace(regex, table)
   await writeFile(path, newReadme)
 }
 
 const [user, token] = process.argv.slice(-2)
-getFollowers(user, token, 3, 6)
+getFollowers(user, token, 6)
 .catch(console.error)
